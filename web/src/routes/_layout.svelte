e5c15be0c3894ee55c90ad8338c073c5b099c079
toey.anirut <toey.anirut@gmail.com>
2022-05-18 23:35:47 +0700

    :tada:

---
+++ web/src/routes/_layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style windi:preflights:global windi:safelist:global>
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
